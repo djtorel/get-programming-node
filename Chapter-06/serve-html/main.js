2f68174cf4f1db9b1ec247e29568840ca052c000
Dominic Torruellas <dom@domtorr.com>
2019-02-14 14:55:24 -0700

    chapter 06 part three

--- Chapter-06/serve-html/main.js
+++ Chapter-06/serve-html/main.js
@@ -2,24 +2,56 @@ const http = require('http');
 const httpStatus = require('http-status');
 const fs = require('fs');
 
-const getViewUrl = url => `views${url}.html`;
+const sendErrorResponse = res => {
+  res.writeHead(httpStatus.NOT_FOUND, {
+    'Content-Type': 'text/html',
+  });
+  res.write('<h1>File Not Found!</h1>');
+  res.end();
+};
 
 const app = http.createServer((req, res) => {
-  let viewUrl = getViewUrl(req.url);
-  fs.readFile(viewUrl, (error, data) => {
-    if (error) {
-      res.writeHead(httpStatus.NOT_FOUND);
-      res.write('<h1>FILE NOT FOUND</h1>');
-      console.log(error);
-    } else {
-      res.writeHead(httpStatus.OK, {
-        'Content-Type': 'text/html',
-      });
-      res.write(data);
-    }
-    res.end();
-  });
+  let url = req.url;
+  if (url.indexOf('.html') !== -1) {
+    res.writeHead(httpStatus.OK, {
+      'Content-Type': 'text/html',
+    });
+    customReadFile(`./views${url}`, res);
+  } else if (url.indexOf('.js') !== -1) {
+    res.writeHead(httpStatus.OK, {
+      'Content-Type': 'text/javascript',
+    });
+    customReadFile(`./public/js${url}`, res);
+  } else if (url.indexOf('.css') !== -1) {
+    res.writeHead(httpStatus.OK, {
+      'Content-Type': 'text/css',
+    });
+    customReadFile(`./public/css${url}`, res);
+  } else if (url.indexOf('.png') !== -1) {
+    res.writeHead(httpStatus.OK, {
+      'Content-Type': 'image/png',
+    });
+    customReadFile(`./public/images${url}`, res);
+  } else {
+    sendErrorResponse(res);
+  }
 });
+
+const customReadFile = (file_path, res) => {
+  if (fs.existsSync(file_path)) {
+    fs.readFile(file_path, (error, data) => {
+      if (error) {
+        console.error(error);
+        sendErrorResponse(res);
+        return;
+      }
+      res.write(data);
+      res.end();
+    });
+  } else {
+    sendErrorResponse(res);
+  }
+};
 
 const port = 3000;
 app.listen(port);
